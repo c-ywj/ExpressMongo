a82bf0b53fe581796a2466ab18f1a1b5dd836b15
Carson Yan <cybc00@hotmail.com>
2019-02-27 09:47:45 -0800

    Adds routes, 2 views, server, and config file

---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Database connection information 
+    'database_cloud': 'not sure yet', 
+    'database': 'college', 
+    // Setting port for server 
+    'port': process.env.PORT || 3000
+}; 
---
+++ server.js
@@ -0,0 +1,34 @@
+var express    = require("express"); 
+var path       = require("path"); 
+var bodyParser = require("body-parser"); 
+var index      = require("./routes/index"); 
+var students   = require("./routes/students"); 
+var config     = require('./config'); 
+
+var app = express(); 
+
+// View engine 
+var ejsEngine = require("ejs-locals"); 
+
+app.engine("ejs", ejsEngine);   // support master pages 
+app.set("view engine", "ejs");  // ejs view engine 
+
+
+// Set static folder.
+//the __dirname is a public variable in node environment, and provides a fully qualified path regardless of where the node environment is hosted
+app.use(express.static(path.join(__dirname, "clients"))); 
+
+
+// Body parser middleware 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: false})); 
+
+ // routes
+app.use("/", index); 
+app.use("/api", students); 
+
+ 
+
+app.listen(config.port, function() { 
+    console.log("Server started on port " + config.port) 
+}); 
